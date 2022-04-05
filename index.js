6d704f53f958cb4aaa6f8c531b94296bcdd5af5a
Mwai <machariamwai@gmail.com>
2022-04-05 12:08:51 +0300

    Concatenate strings

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = "Mwai"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let firstInitial = currentUser[0];
+const shortGreeting = `Welcome, ${firstInitial }!`;
